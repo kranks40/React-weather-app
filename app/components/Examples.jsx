2271f9f8f51e691b8e5a6d72e85552a978481a1a
kranks40 <eng_peters20@yahoo.com>
2020-07-26 12:32:24 -0400

    Added a grid system in main

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,6 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
-var Examples = (props) => <h2>Examples Component</h2>
+var Examples = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few examples locations to try out:</p>  
+
+            <ol>
+                <li><Link to='/?location=Grenada'>St.George, Grenada</Link></li>
+                <li><Link to='/?location=Trinidad'>Port of Spain, Trinidad</Link></li>
+            </ol>  
+        </div>
+       
+    )
+};
 
 
 module.exports = Examples;
