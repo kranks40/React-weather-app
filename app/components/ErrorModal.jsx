4b1cd4c45f7b5552ae0fa2dbeb27b32c5278b989
Oketo Peters <ketoranks29@gmail.com>
2020-07-29 13:12:22 -0400

    Fixed error modal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,30 +1,44 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
+
 
 var ErrorModal = React.createClass({
     getDaultProps: function () {
         return {
-            title: 'Error'
+            title: 'Error',
+            message: 'Wrong city name'
         };
     },
     PropTypes: {
         title: React.PropTypes.string,
-        //message: React.PropTypes.string.isRequired
+        message: React.PropTypes.string.isRequired
     },
     componentDidMount: function () {
+        var {title, message} = this.props;
+        var modalMarkup = (
+            <div id="error-modal" className="reveal tiny text-center" data-reveal>
+                <h4>{title}</h4>
+                <p>{message}</p>
+                
+                <p><button className="button hollow" data-close="">
+                    Okay
+                </button>
+                </p>
+            </div>
+        );
+
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+
     var modal = new Foundation.Reveal($('#error-modal'));
     modal.open();
     } ,   
     render: function () {
-        var {title, message} = this.props;
+        
+
         return (
-            <div id="error-modal" className="reveal tiny text-center" data-reveal="">
-                <h4>{title}</h4>
-                <p>{message}</p>
-                <p>
-                <button className="button hollow" data-close="">
-                    Okay
-                </button>
-                </p>
+            <div>
 
             </div>
         );
